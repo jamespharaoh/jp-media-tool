1a3620302fa5f5e81a980c01d4b888473142182b
James Pharaoh <james@pharaoh.uk>
2024-12-28 07:44:59 +0100

    rename convert command to remaster

--- src/tool/mod.rs
+++ src/tool/mod.rs
@@ -1,7 +1,7 @@
 use clap::Parser as _;
 
-mod convert;
 mod dump;
+mod remaster;
 
 #[ derive (clap::Parser) ]
 struct MainArgs {
@@ -11,14 +11,14 @@ struct MainArgs {
 
 #[ derive (clap::Subcommand) ]
 enum Command {
-	Convert (convert::Args),
 	Dump (dump::Args),
+	Remaster (remaster::Args),
 }
 
 pub fn main () -> anyhow::Result <()> {
 	let main_args = MainArgs::parse ();
 	match main_args.command {
-		Command::Convert (convert_args) => convert::invoke (convert_args),
 		Command::Dump (dump_args) => dump::invoke (dump_args),
+		Command::Remaster (remaster_args) => remaster::invoke (remaster_args),
 	}
 }
